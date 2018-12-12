323039b8c68c0c57b86a809df4a5a0a3b57f0d66
Septdir <septdir@gmail.com>
2018-12-12 17:56:54 +0300

    Base installation package

---
+++ error.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    Protokit Template
+ * @version    @version@
+ * @author     Igor Berdicheskiy - septdir.ru
+ * @copyright  Copyright (c) 2013 - 2018 NorrNext. All rights reserved.
+ * @license    GNU General Public License version 3 or later; see license.txt
+ * @link       https://www.norrnext.com/
+ */
+
+defined('_JEXEC') or die;
+
